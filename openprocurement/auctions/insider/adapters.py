6cc43e7b90f89926ddfa5f40e99e3ca7517aeb88
MotsoVitalii <vitalii.motso@gmail.com>
2017-12-21 12:57:17 +0200

    add adapter

---
+++ openprocurement/auctions/insider/adapters.py
@@ -0,0 +1,6 @@
+# -*- coding: utf-8 -*-
+from openprocurement.auctions.core.adapters import AuctionConfigurator
+
+
+class AuctionInsiderConfigurator(AuctionConfigurator):
+    name = 'Auction Insider Configurator'
