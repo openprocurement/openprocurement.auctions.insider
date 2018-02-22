48440fcc455b245906795f1fd928aee8ce812502
Scandie <grizly.vl@gmail.com>
2018-02-22 14:32:29 +0200

    Reduce amount of overridden views

--- openprocurement/auctions/insider/views/cancellation.py
+++ openprocurement/auctions/insider/views/cancellation.py
@@ -1,13 +1,10 @@
 # -*- coding: utf-8 -*-
-
 from openprocurement.auctions.core.utils import (
     opresource,
-    add_next_award,
 )
 from openprocurement.auctions.dgf.views.financial.cancellation import (
     FinancialAuctionCancellationResource,
 )
-from openprocurement.auctions.insider.constants import TENDER_PERIOD_STATUSES
 
 
 @opresource(name='dgfInsider:Auction Cancellations',
@@ -16,29 +13,4 @@ from openprocurement.auctions.insider.constants import TENDER_PERIOD_STATUSES
             auctionsprocurementMethodType="dgfInsider",
             description="Insider auction cancellations")
 class InsiderAuctionCancellationResource(FinancialAuctionCancellationResource):
-
-    def cancel_auction(self):
-        auction = self.request.validated['auction']
-        if auction.status in TENDER_PERIOD_STATUSES:
-            auction.bids = []
-        auction.status = 'cancelled'
-
-    def cancel_lot(self, cancellation=None):
-
-        if not cancellation:
-            cancellation = self.context
-        auction = self.request.validated['auction']
-        [setattr(i, 'status', 'cancelled') for i in auction.lots if i.id == cancellation.relatedLot]
-        statuses = set([lot.status for lot in auction.lots])
-        if statuses == set(['cancelled']):
-            self.cancel_auction()
-        elif not statuses.difference(set(['unsuccessful', 'cancelled'])):
-            auction.status = 'unsuccessful'
-        elif not statuses.difference(set(['complete', 'unsuccessful', 'cancelled'])):
-            auction.status = 'complete'
-        if 'active.auction' in auction.status and all([
-            i.auctionPeriod and i.auctionPeriod.endDate
-            for i in self.request.validated['auction'].lots
-            if i.numberOfBids > 1 and i.status == 'active'
-        ]):
-            add_next_award(self.request)
+    pass
--- openprocurement/auctions/insider/views/complaint.py
+++ openprocurement/auctions/insider/views/complaint.py
@@ -1,20 +1,10 @@
 # -*- coding: utf-8 -*-
-from openprocurement.api.models import get_now
-from openprocurement.api.utils import (
-    context_unpack,
-    json_view,
-)
 from openprocurement.auctions.core.utils import (
-    apply_patch,
-    check_auction_status,
     opresource,
-    save_auction,
 )
-from openprocurement.auctions.core.validation import validate_patch_complaint_data
 from openprocurement.auctions.dgf.views.financial.complaint import (
     FinancialAuctionComplaintResource,
 )
-from openprocurement.auctions.insider.constants import TENDER_PERIOD_STATUSES
 
 
 @opresource(name='dgfInsider:Auction Complaints',
@@ -23,64 +13,4 @@ from openprocurement.auctions.insider.constants import TENDER_PERIOD_STATUSES
             auctionsprocurementMethodType="dgfInsider",
             description="Insider auction complaints")
 class InsiderAuctionComplaintResource(FinancialAuctionComplaintResource):
-
-    @json_view(content_type="application/json", validators=(validate_patch_complaint_data,), permission='edit_complaint')
-    def patch(self):
-        """Post a complaint resolution
-        """
-        auction = self.request.validated['auction']
-        if auction.status not in TENDER_PERIOD_STATUSES and auction.status not in ['active.qualification', 'active.awarded']:
-            self.request.errors.add('body', 'data', 'Can\'t update complaint in current ({}) auction status'.format(auction.status))
-            self.request.errors.status = 403
-            return
-        if self.context.status not in ['draft', 'claim', 'answered', 'pending']:
-            self.request.errors.add('body', 'data', 'Can\'t update complaint in current ({}) status'.format(self.context.status))
-            self.request.errors.status = 403
-            return
-        data = self.request.validated['data']
-        # complaint_owner
-        if self.request.authenticated_role == 'complaint_owner' and self.context.status in ['draft', 'claim', 'answered', 'pending'] and data.get('status', self.context.status) == 'cancelled':
-            apply_patch(self.request, save=False, src=self.context.serialize())
-            self.context.dateCanceled = get_now()
-        elif self.request.authenticated_role == 'complaint_owner' and auction.status in ['active.tendering'] and self.context.status == 'draft' and data.get('status', self.context.status) == self.context.status:
-            apply_patch(self.request, save=False, src=self.context.serialize())
-        elif self.request.authenticated_role == 'complaint_owner' and auction.status in ['active.tendering'] and self.context.status == 'draft' and data.get('status', self.context.status) == 'claim':
-            apply_patch(self.request, save=False, src=self.context.serialize())
-            self.context.dateSubmitted = get_now()
-        elif self.request.authenticated_role == 'complaint_owner' and self.context.status == 'answered' and data.get('status', self.context.status) == self.context.status:
-            apply_patch(self.request, save=False, src=self.context.serialize())
-        elif self.request.authenticated_role == 'complaint_owner' and self.context.status == 'answered' and data.get('satisfied', self.context.satisfied) is True and data.get('status', self.context.status) == 'resolved':
-            apply_patch(self.request, save=False, src=self.context.serialize())
-        elif self.request.authenticated_role == 'complaint_owner' and self.context.status == 'answered' and data.get('satisfied', self.context.satisfied) is False and data.get('status', self.context.status) == 'pending':
-            apply_patch(self.request, save=False, src=self.context.serialize())
-            self.context.type = 'complaint'
-            self.context.dateEscalated = get_now()
-        elif self.request.authenticated_role == 'auction_owner' and self.context.status == 'claim' and data.get('status', self.context.status) == self.context.status:
-            apply_patch(self.request, save=False, src=self.context.serialize())
-        elif self.request.authenticated_role == 'auction_owner' and self.context.status == 'claim' and data.get('resolution', self.context.resolution) and data.get('resolutionType', self.context.resolutionType) and data.get('status', self.context.status) == 'answered':
-            if len(data.get('resolution', self.context.resolution)) < 20:
-                self.request.errors.add('body', 'data', 'Can\'t update complaint: resolution too short')
-                self.request.errors.status = 403
-                return
-            apply_patch(self.request, save=False, src=self.context.serialize())
-            self.context.dateAnswered = get_now()
-        elif self.request.authenticated_role == 'auction_owner' and self.context.status == 'pending':
-            apply_patch(self.request, save=False, src=self.context.serialize())
-        # reviewers
-        elif self.request.authenticated_role == 'reviewers' and self.context.status == 'pending' and data.get('status', self.context.status) == self.context.status:
-            apply_patch(self.request, save=False, src=self.context.serialize())
-        elif self.request.authenticated_role == 'reviewers' and self.context.status == 'pending' and data.get('status', self.context.status) in ['resolved', 'invalid', 'declined']:
-            apply_patch(self.request, save=False, src=self.context.serialize())
-            self.context.dateDecision = get_now()
-        else:
-            self.request.errors.add('body', 'data', 'Can\'t update complaint')
-            self.request.errors.status = 403
-            return
-        if self.context.tendererAction and not self.context.tendererActionDate:
-            self.context.tendererActionDate = get_now()
-        if self.context.status not in ['draft', 'claim', 'answered', 'pending'] and auction.status in ['active.qualification', 'active.awarded']:
-            check_auction_status(self.request)
-        if save_auction(self.request):
-            self.LOGGER.info('Updated auction complaint {}'.format(self.context.id),
-                        extra=context_unpack(self.request, {'MESSAGE_ID': 'auction_complaint_patch'}))
-            return {'data': self.context.serialize("view")}
+    pass
