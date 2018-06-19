4d5b8d104b0be7f89b73c26b96e4d4074239ff7e
vitalii-motso <vitalii.motso@gmail.com>
2018-06-19 13:01:02 +0300

    mock config for testing (#88)

---
+++ openprocurement/auctions/insider/tests/fixtures.py
@@ -0,0 +1,16 @@
+MOCK_CONFIG_PARTIAL_PLUGINS = {
+    "auctions.insider":{
+        "use_default":True,
+        "plugins":{
+            "insider.migration":None
+        },
+        "migration":False,
+        "aliases":[]
+    }
+}
+
+MOCK_CONFIG_PARTIAL_AUCTION = {
+    "url": "http://auction-sandbox.openprocurement.org",
+    "public_key": "fe3b3b5999a08e68dfe62687c2ae147f62712ceace58c1ffca8ea819eabcb5d1"
+    }
+
